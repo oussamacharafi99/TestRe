c35d90749e1f95c0fdfca7b7d712d5713b18a735
oussama charafi <ou.charafi@gmail.com>
2024-08-28 22:55:25 +0100

    I added the config and discovery and gateway

---
+++ gateway-service/src/main/java/org/example/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,22 @@
+package org.example.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rd, DiscoveryLocatorProperties dl){
+        return new DiscoveryClientRouteDefinitionLocator(rd,dl);
+    }
+
+}
